6df8a4e7a423208e1c5a29e603421649b9ba83dc
Bbenekko <bbeneko@outlook.com>
2025-10-19 19:58:44 -0300

    Ax funfando

--- trab1/ax.c
+++ trab1/ax.c
@@ -11,6 +11,7 @@
 
 #include "kernelSim.h"
 
+#define FIFO_AX_OUT "axFifoOut"
 
 #define D1 1
 #define D2 0
@@ -24,8 +25,44 @@ int MAX = 40;
 int Dx;
 int Op;
 
+int outAxFIFO;
+
+void sysCall(int dispositivo, char operacao)
+{
+    // pid
+    char msg_to_kernel1[16];
+    sprintf(msg_to_kernel1, "%d", getpid());
+    write(outAxFIFO, msg_to_kernel1, strlen(msg_to_kernel1));
+
+    // PC
+    char msg_to_kernel2[3];
+    sprintf(msg_to_kernel2, "%d", PC);
+    write(outAxFIFO, msg_to_kernel2, strlen(msg_to_kernel2));
+
+    // Dispositivo
+    char msg_to_kernel3;
+    sprintf(msg_to_kernel3, "%d", dispositivo);
+    write(outAxFIFO, msg_to_kernel3, strlen(msg_to_kernel3));
+
+    // Operação
+    write(outAxFIFO, operacao, 1);
+}
+
 int main(void)
 {
+    if (access(FIFO_AX_OUT, F_OK) == -1)
+    {
+        if (mkfifo (FIFO_AX_OUT, S_IRUSR | S_IWUSR) != 0)
+        {
+            fprintf (stderr, "Erro ao criar FIFO %s\n", FIFO_AX_OUT);
+            return -1;
+        }
+    }
+    if ((outAxFIFO = open (FIFO_AX_OUT, O_RDONLY)) < 0)
+    {
+        fprintf (stderr, "Erro ao abrir a FIFO %s\n", FIFO_AX_OUT);
+        return -2;
+    }
 
      //area da memoria compartilhada para compartilhar os status dos processos
     __key_t chave = 8751;
@@ -43,8 +80,10 @@ int main(void)
             else Op = X;
             sysCall (Dx, Op);
         }
+        else sysCall(0, '\n');
         sleep(0.1);
         PC++;
-        printf("%d", PC);
+        //printf("%d", PC);
     }
+    close(outAxFIFO);
 }
