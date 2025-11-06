676ddfe411159230c5a8b548f3946630c06b648e
Bbenekko <bbeneko@outlook.com>
2025-11-05 21:28:11 -0300

    ex 2 finalizado

--- lab8/ex2/ex2.c
+++ lab8/ex2/ex2.c
@@ -1,4 +1,3 @@
-/* Exemplo de uso de unico semáforo*/
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,17 +24,38 @@ union semun
     unsigned short *array;
 };
 
+// inicializa o valor do semáforo
+int setSemValue(int vecSemId, int semId, int value);
+// remove o semáforo
+void delSemValue(int semId);
+// operação P
+int semaforoP(int vecSemId, int semId);
+// operação V
+int semaforoV(int vecSemId, int semId);
+
 void zeraBuffer(char* buffer);
 
 int main(void)
 {
-    int segmento, id;
+    int segmento, id, vecSemId;
+    int semPro = 0;
+    int semImp = 1;
     char* buffer;
 
     segmento = shmget(IPC_PRIVATE, sizeof(char) * BUFFER_SIZE , IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
 
     buffer = (char *)shmat(segmento, 0, 0);
     zeraBuffer(buffer);
+
+    vecSemId = semget(8751, 2, 0666 | IPC_CREAT);
+    if (vecSemId == -1)
+    {
+        puts( "Erro na criação do  semáforo!");
+        exit(-1);
+    }
+    
+    setSemValue(vecSemId, semPro, 1);
+    setSemValue(vecSemId, semImp, 0);
 
     if ((id = fork()) < 0)
     {
@@ -44,34 +64,40 @@ int main(void)
     }
     else if (id == 0) // processo filho - produtor
     {
+        char c;
         while(TRUE)
         {
+            semaforoP(vecSemId, semPro);
             for(int i = 0; i < BUFFER_SIZE; i++)
             {
-                printf(BLUE"leitor -"RESET" Insira um caracter (apenas um!!!):\n");
-                char c = getchar();
+                printf(BLUE"leitor -"RESET" Insira um caracter (apenas um!!!) na posição " GREEN"%d"RESET":\n", i);
+                c = getchar();
+                while(getchar() != '\n'); // consome lixo (e o enter) após o caracter!
                 buffer[i] = c;
-                printf(BLUE"leitor -"RESET" Caracter inserido na posicao "GREEN"%d"RESET" do buffer!\n", i);
             }
+            semaforoV(vecSemId, semImp);
         }
     }
     else //              processo pai - impressor
     {
         while(TRUE)
         {
-            if(buffer[BUFFER_SIZE-1] != NULL)
-            {
-                printf(YELLOW"impressor -"RESET" Entrou em execução...\n");
-                printf(YELLOW"impressor -"RESET" Conteúdo do buffer : "GREEN"["RESET);
+            semaforoP(vecSemId, semImp);
 
-                for(int i = 0; i < BUFFER_SIZE; i++)
-                {
-                    printf(GREEN"%c, "RESET, buffer[i]);
-                }
-                printf(GREEN"]\n"RESET);
-                // stuff
-                zeraBuffer(buffer);
+            printf(YELLOW"impressor -"RESET" Entrou em execução...\n");
+            printf(YELLOW"impressor -"RESET" Conteúdo do buffer : "GREEN"["RESET);
+
+
+            for(int i = 0; i < BUFFER_SIZE; i++)
+            {
+                printf(GREEN"%c"RESET, buffer[i]);
+                if (i < BUFFER_SIZE-1) printf(", ");
             }
+            printf(GREEN"]\n"RESET);
+
+            zeraBuffer(buffer);
+
+            semaforoV(vecSemId, semPro);
         }
     }
 
@@ -82,6 +108,39 @@ void zeraBuffer(char* buffer)
 {
     for(int i = 0; i < BUFFER_SIZE; i++ )
     {
-        buffer[i] = NULL;
+        buffer[i] = '\0';
     }
+}
+
+int setSemValue(int vecSemId, int semId, int value)
+{
+    union semun semUnion;
+    semUnion.val = value;
+    return semctl(vecSemId, semId, SETVAL, semUnion);
+}
+
+void delSemValue(int semId)
+{
+    union semun semUnion;
+    semctl(semId, 0, IPC_RMID, semUnion);
+}
+
+int semaforoP(int vecSemId, int semId)
+{
+    struct sembuf semB;
+    semB.sem_num = semId;
+    semB.sem_op = -1;
+    semB.sem_flg = SEM_UNDO;
+    semop(vecSemId, &semB, 1);
+    return 0;
+}
+
+int semaforoV(int vecSemId, int semId)
+{
+    struct sembuf semB;
+    semB.sem_num = semId;
+    semB.sem_op = 1;
+    semB.sem_flg = SEM_UNDO;
+    semop(vecSemId, &semB, 1);
+    return 0;
 }
