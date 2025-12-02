179f7c7425cb445e68e17135c4598d7505ae44d2
Bbenekko <bbeneko@outlook.com>
2025-12-02 00:47:50 -0300

    debug e falta só A0 e testar

--- trab2/bento.c
+++ trab2/bento.c
@@ -3,13 +3,15 @@
 #include <string.h>
 #include <sys/stat.h>
 
+//TODO            LÓGICA DO A0 e testagem com a main da maya
+//TODO            LÓGICA DO A0 e testagem com a main da maya
+//TODO            LÓGICA DO A0 e testagem com a main da maya
+
+
 #include "structs.h"
 
 void create_subDirectory(Message* dc_req, Message* dc_rep)
 {
-    // TODO função checa validade do path
-
-    // TODO talvez tenhamos que adicionar um path relativo ou algo do tipo
 
     char aux[PATH_MAX];
 
@@ -19,7 +21,7 @@ void create_subDirectory(Message* dc_req, Message* dc_rep)
     {
         // path estourou o buffer ou snprintf inválido
 
-        // TODO CODIGO DE ERRO
+        perror("ERRO: create_subDirectory - Path estourou o buffer ou snprintf invalido");
 
         dc_rep->sizePathName = -1;
         return;
@@ -42,7 +44,7 @@ void create_subDirectory(Message* dc_req, Message* dc_rep)
         // erro de criação de diretório
         dc_rep->sizePathName = -1;
 
-        // TODO código de erro
+        perror("ERRO: create_subdirectory - Erro na criação de diretório!");
 
         return;
     }
@@ -50,14 +52,11 @@ void create_subDirectory(Message* dc_req, Message* dc_rep)
 
 void write_file(Message* wr_req, Message* wr_rep)
 {
-    // TODO função checa validade do path
-
-    // TODO talvez tenhamos que adicionar um path relativo ou algo do tipo
 
     // checa offset
     if(wr_req->offset % 16 != 0)
     {
-        // TODO printa erro 
+        perror("ERRO: write_file - Offset inválido, não é múltiplo de 16");
         wr_rep->offset = -1;
         return;
     }
@@ -68,6 +67,7 @@ void write_file(Message* wr_req, Message* wr_rep)
         if(remove(wr_req->pathName)) // path relativo faltando  
         {
             // TODO erro ao deletar
+            perror("ERRO: write_file - erro ao deletar arquivo!");
             wr_rep->offset = -1;
             return;
         }
@@ -85,8 +85,9 @@ void write_file(Message* wr_req, Message* wr_rep)
     int size_original;
     char ch;
 
-    if ((original = fopen(wr_req->pathName, "r")) == NULL) {
-        perror("Error opening original file");
+    if ((original = fopen(wr_req->pathName, "rw")) == NULL) 
+    {
+        perror("ERRO: write_file - Error opening original file");
         wr_rep->offset = -1;
         return;
     }
@@ -100,7 +101,7 @@ void write_file(Message* wr_req, Message* wr_rep)
     {
         if ((temporario = fopen("temp.txt", "a")) == NULL) 
         {
-            perror("Error opening temporary file for writing");
+            perror("ERRO: write_file - Error opening temporary file for writing");
             wr_rep->offset = -1;
             return;
         }
@@ -135,31 +136,7 @@ void write_file(Message* wr_req, Message* wr_rep)
         return;
 
     }
-    /*
-    else if(wr_req->payload == size_original)
-    {
-        fclose(original);
-        if ((original = fopen(wr_req->pathName, "w")) == NULL) 
-        {
-            perror("Error opening original file for appending");
-            wr_rep->offset = -1;
-            return;
-        }
-
-        fputs(wr_req->payload, original);
-
-        fclose(original);
-
-        wr_rep->owner = wr_req->owner;
-        strcpy(wr_rep->pathName, wr_req->pathName);
-        wr_rep->sizePathName = wr_req->pathName;
-        strcpy(wr_rep->payload, "");
-        wr_rep->offset = wr_req->offset;
-
-        return;
-    }
-    */
-    else // offset maior ou igual que o do arquivo
+       else // offset maior ou igual que o do arquivo
     {
         fclose(original);
         if ((original = fopen(wr_req->pathName, "a")) == NULL) 
@@ -181,8 +158,8 @@ void write_file(Message* wr_req, Message* wr_rep)
         fclose(original);
 
         wr_rep->owner = wr_req->owner;
-        // TODO talvez trocar para strncpy por segurança?? estudar um pouco a fundo sobre isso
-        strcpy(wr_rep->pathName, wr_req->pathName);
+        strncpy(wr_rep->pathName, wr_req->pathName, wr_rep->sizePathName - 1);
+        wr_rep->pathName[wr_rep->sizePathName - 1] = '\0'; // Garantir terminação
         wr_rep->sizePathName = wr_req->sizePathName;
         strcpy(wr_rep->payload, "");
         wr_rep->offset = wr_req->offset;
